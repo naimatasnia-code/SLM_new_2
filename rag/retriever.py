f2072a6c929eb5453cab717e788570bd56b78af0
Naima Tasnia <66075177+Naima-ai@users.noreply.github.com>
2026-01-17 16:44:14 +0100

    Create retriever.py

---
+++ rag/retriever.py
@@ -0,0 +1,16 @@
+
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+def load_retriever(vector_dir: str, top_k: int):
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
+
+    db = FAISS.load_local(
+        vector_dir,
+        embeddings,
+        allow_dangerous_deserialization=True
+    )
+
+    return db.as_retriever(search_kwargs={"k": top_k})
