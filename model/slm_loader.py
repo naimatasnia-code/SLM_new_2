e9bd03c968c4a910d97eebede684a7d6bc14a7eb
Naima Tasnia <66075177+Naima-ai@users.noreply.github.com>
2026-01-26 00:10:35 +0100

    Update slm_loader.py

--- model/slm_loader.py
+++ model/slm_loader.py
@@ -1,18 +1,32 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from peft import PeftModel
+import torch
 
 MODELS = {
     "phi-2": "microsoft/phi-2",
-    "mistral": "mistralai/Mistral-7B-Instruct-v0.1"
+    "tinyllama": "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 }
 
-def load_slm(model_name: str):
+def load_slm(
+    model_name: str,
+    lora_path: str | None = None,
+    quantized: bool = True
+):
+
+    torch.set_grad_enabled(False)
     model_id = MODELS[model_name]
 
     tokenizer = AutoTokenizer.from_pretrained(model_id)
+
     model = AutoModelForCausalLM.from_pretrained(
         model_id,
-        device_map="auto"
+        device_map="auto",
+        load_in_4bit=quantized,   # 
+        torch_dtype=torch.float16
     )
+
+    if lora_path:
+        model = PeftModel.from_pretrained(model, lora_path)
 
     model.eval()
     return tokenizer, model
