260a7ed2a50794f244d339ff899bd703c5f4edd9
Naima Tasnia <66075177+Naima-ai@users.noreply.github.com>
2026-02-02 17:57:05 +0100

    Add model registry import in universal_loader.py

--- model/universal_loader.py
+++ model/universal_loader.py
@@ -1,7 +1,9 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
+from model.model_registry import MODEL_REGISTRY
 
 def load_model(model_name, lora_path=None):
+    model_name = MODEL_REGISTRY.get(model_name, model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(model_name)
 
