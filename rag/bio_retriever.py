8b194e78a266bf0c4293607e8126aac1f2d4f0cd
naimatasnia-code <naima.tasnia@studenti.unipd.it>
2026-03-02 21:31:30 +0100

    Update bio_retriever.py

--- rag/bio_retriever.py
+++ rag/bio_retriever.py
@@ -4,7 +4,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from rag_retriever import BioRAG
+from rag.rag_retriever import BioRAG
 from langchain.schema import Document
 
 
